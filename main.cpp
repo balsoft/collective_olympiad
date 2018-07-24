f01509d506098eb3e11826165bae41e41cea701a
Александр Бантьев <balsoft@yandex.ru>
2018-07-24 16:52:03 +0300

    Input done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+#include <string>
+
+using namespace std;
+
 int main(int argc, char **argv) {
+    int N, S, C, H, U;
+    cin >> N >> S >> C >> H >> U;
+    string temp;
+    temp.resize(N);
+    string hand;
+    hand.resize(H + S);
+    cin.read(&temp[0], N);
+    cin.read(&hand[0], H+S);
     
     return 0;
 }
