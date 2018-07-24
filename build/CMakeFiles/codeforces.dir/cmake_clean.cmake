89767241fed495a6afd805d317d45c81942ae918
Александр Бантьев <balsoft@yandex.ru>
2018-07-24 15:56:27 +0300

    Initial commit

---
+++ build/CMakeFiles/codeforces.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/codeforces.dir/main.cpp.o"
+  "codeforces.pdb"
+  "codeforces"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/codeforces.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
